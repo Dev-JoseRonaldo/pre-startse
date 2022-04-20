a1e69b8494bc5c82ba451a2bff74425e75d264c4
José Ronaldo <dev.joseronaldo@gmail.com>
2022-04-20 18:51:11 -0300

    seção 'todos os projetos' (owl carousel)

--- script/setup_owl.js
+++ script/setup_owl.js
@@ -1,17 +1,19 @@
 $('#slider').owlCarousel({
+  center: true,
   loop: true,
-  margin: 30,
+  margin: 15,
   nav: false,
   autoplay: true,
   responsive: {
     0: {
       items: 1
     },
-    800: {
-      items: 2
+    1100: {
+      items: 1
     },
-    1200: {
-      items: 3
+    1400: {
+      items: 2
     }
   }
 })
+
