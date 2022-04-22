ffadf81e781740bf34f911524ad05fed4b4d004c
José Ronaldo <dev.joseronaldo@gmail.com>
2022-04-22 16:38:25 -0300

    Seção depoimentos e ajustes

--- script/setup_owl.js
+++ script/setup_owl.js
@@ -17,3 +17,23 @@ $('#slider').owlCarousel({
   }
 })
 
+$('#slider2').owlCarousel({
+  center: true,
+  loop: true,
+  margin: 15,
+  nav: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    1100: {
+      items: 1
+    },
+    1400: {
+      items: 1.7
+    }
+  }
+})
+
+
