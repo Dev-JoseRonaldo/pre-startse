906d225de8123b2be1bafa852e63588c1b1e1731
José Ronaldo <dev.joseronaldo@gmail.com>
2022-04-25 17:27:05 -0300

    Responsividade Pagina de inscrição

--- script/toggle.js
+++ script/toggle.js
@@ -6,7 +6,6 @@ const menuToggle = document.querySelector('.menu-toggle');
 const buttonsToggle = document.querySelectorAll('header .content-header .menu-section nav ul li');
 
 
-
 menuToggle.addEventListener("click", () => {
   document.body.style.overflow = show ? "hidden" : "initial";
 
