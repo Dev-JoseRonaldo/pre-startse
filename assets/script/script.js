a064c99a886e0b7ee2bb5e5c2f7d750ed81567be
José Ronaldo <dev.joseronaldo@gmail.com>
2022-05-04 20:57:35 -0300

    Separando e organizando os arquivos CSS

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,12 @@
 import Countdown from "./countdown.js";
 
-const tempoFuturo = new Countdown("27 april 2022 22:00:00 GMT-0300");
+const tempoFuturo = new Countdown("15 may 2022 19:00:00 GMT-0300");
 const tempos = document.querySelectorAll("[data-time]");
 
 const relogio = document.querySelector('.countdown');
 const imgbox = document.querySelector('.img-box');
 
 function mostrarTempo() {
-
   tempos.forEach((tempo, index) => {
     tempo.innerHTML = tempoFuturo.total[index];
   });
