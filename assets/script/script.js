1bbcf15d2b3f4069d801c2e14e2ef0552b228602
José Ronaldo <dev.joseronaldo@gmail.com>
2022-05-10 16:59:16 -0300

    Separando as paginas da primeira e segunda edição

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,16 @@
-import Countdown from "./countdown.js";
+import Countdown from './countdown.js'
 
-const tempoFuturo = new Countdown("15 may 2022 19:00:00 GMT-0300");
-const tempos = document.querySelectorAll("[data-time]");
+const tempoFuturo = new Countdown('30 may 2022 19:00:00 GMT-0300')
+const tempos = document.querySelectorAll('[data-time]')
 
-const relogio = document.querySelector('.countdown');
-const imgbox = document.querySelector('.img-box');
+const relogio = document.querySelector('.countdown')
+const imgbox = document.querySelector('.img-box')
 
 function mostrarTempo() {
   tempos.forEach((tempo, index) => {
-    tempo.innerHTML = tempoFuturo.total[index];
-  });
+    tempo.innerHTML = tempoFuturo.total[index]
+  })
 }
 
-mostrarTempo();
-setInterval(mostrarTempo, 1000);
-
-
-
-
+mostrarTempo()
+setInterval(mostrarTempo, 1000)
