269b1f32c2f33ef57bd49e742186bcbb20873cc7
José Ronaldo <dev.joseronaldo@gmail.com>
2022-07-04 21:30:52 -0300

    :recycle: refactor: atualizando data

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,7 @@ import Countdown from './countdown.js'
 
 var dia = 7
 
-const tempoFuturo = new Countdown(`7 july 2022 00:53:00 GMT-0300`)
+const tempoFuturo = new Countdown(`10 octuber 2022 00:53:00 GMT-0300`)
 const tempos = document.querySelectorAll('[data-time]')
 
 const relogio = document.querySelector('.countdown')
